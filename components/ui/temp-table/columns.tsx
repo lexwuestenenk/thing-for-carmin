814441db22897b025ef1c39ddafa138dd969b7ab
lexwuestenenk <lex.wuestenenk@hotmail.com>
2024-10-06 17:01:36 +0200

    Change column header

--- components/ui/temp-table/columns.tsx
+++ components/ui/temp-table/columns.tsx
@@ -46,7 +46,7 @@ export const columns : ColumnDef<Product>[] = [
                 variant="ghost"
                 onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
                 >
-                    Price
+                    Price (Total)
                     <ArrowUpDown className="ml-2 h-4 w-4" />
                 </Button>
             )
